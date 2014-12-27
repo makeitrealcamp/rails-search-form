4274a4d300d1d9a890fc2b19e75ee1d150f6126e
German Escobar <german.escobarc@gmail.com>
2014-12-27 02:15:32 -0500

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+restaurantes = Category.create(name: "Restaurantes")
+carro = Category.create(name: "Carro")
+Category.create(name: "Hogar")
+
+Expense.create(category: restaurantes, date: Date.current, concept: "El Corral", amount: 32000)
+Expense.create(category: restaurantes, date: Date.current - 4, concept: "El Corral", amount: 45000)
+Expense.create(category: carro, date: Date.current, concept: "Gasolina", amount: 80000)
